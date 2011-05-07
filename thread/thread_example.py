b424ed2a1d6cb6ca63b41029458eb623948cba54
Samuel <samuelololol@gmail.com>
2011-05-07 20:42:29 +0800

    an example of an thread app

---
+++ thread/thread_example.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import threading
+import Queue
+
+thread_numbers = 4
+
+class mythread(threading.Thread):
+    def run(self):
+        global Pool
+        tname = threading.currentThread().getName()
+        while True:
+            item = Pool.get()
+            if item != None:
+                if item == 0:
+                    break
+                work(item,tname)
+            Pool.task_done()
+        Pool.task_done()
+
+def work(item,name):
+    global mutex
+    mutex.acquire()
+    print item,name
+    mutex.release()
+
+def main():
+    global Pool, mutex, thread_numbers
+    mutex = threading.Lock() 
+    threads = []
+    Pool = Queue.Queue(0)
+
+    # example data
+    ha = [1,2,3,4,5,6,7,8,9]
+
+    # create threads
+    for x in range(thread_numbers):
+        threads.append(mythread())
+
+    # all threads start
+    for t in threads:
+        t.start()
+
+    # push item in to FIFO queue: Pool
+    for item in ha:
+        Pool.put(item)
+
+    # add the leave condition for each thread
+    for idx in range(thread_numbers):
+        Pool.put(0)
+
+    Pool.join()
+
+if __name__ == '__main__':
+    main()
